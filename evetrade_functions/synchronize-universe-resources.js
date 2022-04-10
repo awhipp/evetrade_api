d466b876f2f01a66c12bd2fa597e1da3a98d14a4
Alexander Whipp <alexruns4@gmail.com>
2022-04-09 20:02:31 -0400

    Splits JSON Resource File into smaller components
    More responsive when queried by other lambda functions

--- evetrade_functions/synchronize-universe-resources.js
+++ evetrade_functions/synchronize-universe-resources.js
@@ -1,3 +1,5 @@
+// Synchronizes the EVE Universe Resource in S3
+
 const https = require('https');
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
@@ -12,54 +14,55 @@ const options = {
     }
 };
 
-const payload = {};
-
 function getRequest(url, options) {
-  
+    
     return new Promise((resolve, reject) => {
-      const req = https.get(url, options, res => {
-        let rawData = '';
-  
-        res.on('data', chunk => {
-            rawData += chunk;
+        const req = https.get(url, options, res => {
+            let rawData = '';
+            
+            res.on('data', chunk => {
+                rawData += chunk;
+            });
+            
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(rawData));
+                } catch (err) {
+                    reject(new Error(err));
+                }
+            });
         });
-  
-        res.on('end', () => {
-          try {
-            resolve(JSON.parse(rawData));
-          } catch (err) {
+        
+        req.on('error', err => {
             reject(new Error(err));
-          }
         });
-      });
-  
-      req.on('error', err => {
-        reject(new Error(err));
-      });
     });
-  }
-  
+}
+
+async function uploadToS3(bucket, key, body, contentType) {
+    const UPLOAD_PARAMS = {
+        Bucket: bucket,
+        Key: key,
+        Body: body, 
+        ContentType: contentType
+    }
+    
+    console.log(`Uploading to ${UPLOAD_PARAMS.Bucket} ${UPLOAD_PARAMS.Key}`);
+    
+    await s3.upload(UPLOAD_PARAMS)
+    .promise()
+    .then( data => { console.log(`Success`, data); })
+    .catch( err => { console.log(`Failed`, err); })
+}
 
 exports.handler = async function(event, context) {    
     console.log(`Sending Request to ${RES_ENDPOINT}`);
-
+    
     const data = await getRequest(RES_ENDPOINT, options);
-
+    
     for (var i = 0; i < data.length; i++) {
-        payload[data[i].name] = await getRequest(data[i].download_url, options);
+        body = await getRequest(data[i].download_url, options);
+
+        await uploadToS3('evetrade', `resources/${data[i].name}`, JSON.stringify(body), 'application/json');
     }
-
-    const UPLOAD_PARAMS = {
-      Bucket: 'evetrade',
-      Key: 'resources/universe_information.json',
-      Body: JSON.stringify(payload), 
-      ContentType: "application/json"
-    }
-
-    console.log(`Uploading EVETrade Resources to ${UPLOAD_PARAMS.Bucket} ${UPLOAD_PARAMS.Key}`);
-
-    await s3.upload(UPLOAD_PARAMS)
-      .promise()
-      .then( data => { console.log(`Success`, data); })
-      .catch( err => { console.log(`Failed`, err); })
 }
