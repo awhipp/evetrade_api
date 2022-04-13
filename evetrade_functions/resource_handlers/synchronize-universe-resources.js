87d9d42605015134c734dffce629fd70572814e3
Alexander Whipp <alexruns4@gmail.com>
2022-04-13 19:21:46 -0400

    Generates additional static resource artifacts
    * systemIdToSecurity.json
    * typeIDtoName.json

--- evetrade_functions/resource_handlers/synchronize-universe-resources.js
+++ evetrade_functions/resource_handlers/synchronize-universe-resources.js
@@ -14,7 +14,7 @@ const options = {
     }
 };
 
-function getRequest(url, options) {
+function get_request(url, options) {
     
     return new Promise((resolve, reject) => {
         const req = https.get(url, options, res => {
@@ -39,30 +39,77 @@ function getRequest(url, options) {
     });
 }
 
-async function uploadToS3(bucket, key, body, contentType) {
+function get_security_code(security) {
+    if (security >= 0.5) {
+        return "high_sec";
+    } else if (security > 0) {
+        return "low_sec";
+    } else if (security <= 0) {
+        return "null_sec";
+    } else {
+        return -1;
+    }
+}
+
+async function upload_to_s3(bucket, key, body, contentType) {
     const UPLOAD_PARAMS = {
         Bucket: bucket,
         Key: key,
         Body: body, 
         ContentType: contentType
-    }
+    };
     
     console.log(`Uploading to ${UPLOAD_PARAMS.Bucket} ${UPLOAD_PARAMS.Key}`);
     
     await s3.upload(UPLOAD_PARAMS)
     .promise()
     .then( data => { console.log(`Success`, data); })
-    .catch( err => { console.log(`Failed`, err); })
+    .catch( err => { console.log(`Failed`, err); });
+
+    if (key.indexOf('invTypes') > 0) {
+        const json = JSON.parse(body.toString('utf-8'));
+        const typeIDToName = {};
+        for (const obj in json) {
+            const metadata = json[obj];
+            const typeId = metadata["typeID"];
+            const name = metadata["typeName"];
+            const volume = metadata["volume"];
+            typeIDToName[typeId] = {
+                "name": name,
+                "volume": volume
+            };
+        }
+
+        await upload_to_s3('evetrade', 'resources/typeIDToName.json', JSON.stringify(typeIDToName), 'application/json');
+    }
+
+    if (key.indexOf('universeList') > 0) {
+        const json = JSON.parse(body.toString('utf-8'));
+        const systemIdToSecurity = {};
+        for (const obj in json) {
+            const metadata = json[obj];
+            const systemId = metadata["system"];
+            const security = metadata["security"];
+            const securityCode = get_security_code(security);
+
+            systemIdToSecurity[systemId] = {
+                "rating": security,
+                "security_code": securityCode
+            };
+        }
+
+        await upload_to_s3('evetrade', 'resources/systemIdToSecurity.json', JSON.stringify(systemIdToSecurity), 'application/json');
+    }
 }
 
 exports.handler = async function(event, context) {    
     console.log(`Sending Request to ${RES_ENDPOINT}`);
     
-    const data = await getRequest(RES_ENDPOINT, options);
+    const data = await get_request(RES_ENDPOINT, options);
     
     for (var i = 0; i < data.length; i++) {
-        body = await getRequest(data[i].download_url, options);
+        const body = await get_request(data[i].download_url, options);
 
-        await uploadToS3('evetrade', `resources/${data[i].name}`, JSON.stringify(body), 'application/json');
+        await upload_to_s3('evetrade', `resources/${data[i].name}`, JSON.stringify(body), 'application/json');
     }
-}
+};
