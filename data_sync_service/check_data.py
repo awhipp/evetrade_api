802c91d5bae79fec6071628030a26173194fc429
Alexander Whipp <alexruns4@gmail.com>
2022-08-17 20:56:57 -0400

    Removing dependency on data_log index

--- data_sync_service/check_data.py
+++ data_sync_service/check_data.py
@@ -12,35 +12,31 @@ ES_HOST = os.environ['ES_HOST']
 
 es_client = Elasticsearch(ES_HOST)
 
-def get_recent_values(index_name, limit):
+def get_recent_values(index_name):
     '''
     Gets the most recent values from the given index
     '''
     query = {
+        "size": 1,
+        "sort": { "issued": "desc" },
         "query": {
-            "bool": {
-                "filter": {
-                    "range": {
-                        "epoch_end": {
-                            "gt": limit
-                        }
-                    }
-                }
-            }
+            "match_all": {}   
         }
     }
 
     hits = es_client.search(index=index_name, body=query)
     return hits['hits']['hits']
 
+ten_minutes_ago = time.time() - (10 * 60)
 
-print(f'Getting recent values from {time.time() - 60*10}')
+print(f'Getting most recent document. 10 minutes ago was: {time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(ten_minutes_ago))}')
 
-results = get_recent_values('data_log', time.time() - 60*10)
+results = get_recent_values('market_data')
+last_order_time = int(results[0]['sort'][0])/1000
 
-if len(results) > 0:
-    print(f'Found {len(results)} results')
-else:
+print(f'Most recent document was from from: {time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(last_order_time))}')
+
+if time.time() - 60*10 > last_order_time:
     raise Exception('No new data ingested into Elasticsearch index for the last 10 minutes.')
 
 status_code = urllib.request.urlopen('https://evetrade-api.herokuapp.com/').getcode()
